a7f36a32168deea0dca858cd779b36a5a26ef8f5
MARIA PAULA LLANO BRAVO <maria.llano@uao.edu.co>
2024-09-03 19:21:46 -0500

    variables, tipos de datos y arreglos

--- basic.js
+++ basic.js
@@ -1,1 +1,31 @@
 console.log("alabio alabao alabin bom bao")
+
+// Variable
+// let -> indica que puede cambiar a lo largo del programa
+// const -> se mantiene
+let nombreDeLaVariable = "String"
+const variableConst ="String2"
+
+// Tipos de datos
+
+let variable="string"
+console.log(variable)
+let numero=25
+console.log(numero)
+let variableNula=null // yo la pongo
+let variableNoDefinida =undefined // el sistema la pone
+let simbolo= Symbol() // no cambia en todo el programa
+
+// Estructuras de datos
+// ARRAYS
+let escuderias= ["red bull","hass","alpine","ferrari"]
+console.log(escuderias)
+console.log(escuderias.length)
+
+// Objetos
+let carro={
+    marca:'porche',
+    modelo:'2009',
+    valor:12345
+}
+console.log(carro)
