f68c8a5c5fcdbf8633d4b381fdd287feaf3d500f
MARIA PAULA LLANO BRAVO <maria.llano@uao.edu.co>
2024-09-03 20:07:22 -0500

    condicionales y ciclos

---
+++ ciclos.js
@@ -0,0 +1,36 @@
+// Switch
+let expr ="Maracuya"
+switch(expr){
+    case "Naranja":
+        console.log("Son Naranjas :D")
+        break;
+    case "Lulo":
+        console.log("Son Lulos :D")
+        break;
+    default:
+        console.log("No es ni Naranja ni Lulo :(")
+    break;
+}
+
+// While
+let contador=0;
+while (contador<10){
+    console.log(contador)
+    contador++;
+}
+
+// Do- While
+let counter =0
+do{
+    console.log(counter)
+    counter ++;
+}while (counter<10);
+
+// For
+let frutas=["apple","orange","watermelon","grape"]
+for (let i=0; i<frutas.length; i++){
+    console.log(frutas[i])
+}
+// For-In
+
+// For-Of
---
+++ condicionales.js
@@ -0,0 +1,9 @@
+let nombre="Paula"
+
+if(nombre ==="mauricio"){
+    console.log("hola rarito")
+}else if (nombre ==="laura") {
+    console.log("hola laura")
+}else{
+    console.log("hola palula")
+}
